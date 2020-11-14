713f17a5c3d028e3eb68887129b284b3da41a648
Matthew-Jordon Whynter <mattwhynt@yahoo.com>
2020-11-14 18:20:34 -0500

    Exercise 4

--- world.js
+++ world.js
@@ -1,12 +1,13 @@
 window.onload = function () {
     var btn = document.getElementById("lookup");
     var request = new XMLHttpRequest();
+    var result = document.getElementById("result");
     btn.addEventListener("click", function() {
         var search = document.getElementById("country").value;
         request.open("GET","http://localhost:8080/world.php?q=" + search,true);
         request.onreadystatechange = function () {
             if (this.DONE && this.status == 200) {
-                document.getElementById("result").innerHTML = request.responseText;
+                result.innerHTML = request.responseText;
             }
         };
         request.send();
--- world.php
+++ world.php
@@ -12,9 +12,9 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+<table style = 'width: 100%', 'text-align: left'>
+  <tr><th>Name</th><th>Continent</th><th>Independence</th><th>Head of State</th></tr>
 <?php foreach ($results as $row): ?>
-  <br>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <tr><td><?= $row['name'].'</td><td>'.$row['continent'].'</td><td>'.$row['independence_year'].'</td><td>'.$row['head_of_state'];?></td></tr>
 <?php endforeach; ?>
-</ul>
+</table>
